1679684ebff4c5c4a1ec803d98be77257c73ec1d
rayron5656 <86421133+rayron5656@users.noreply.github.com>
2022-01-10 15:39:50 +0200

    Add files via upload

---
+++ Proxy HW/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Proxy_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    interface IPersonal
+    {
+        void changePassport();
+        void GetMyInfo();
+        void ChangeAddress();
+
+    }
+
+    interface IAdminastry
+    {
+        void GetEveryoneinfo();
+        void NotSomethingThatShouldBeAvilableForEveryone();
+    }
+
+
+
+    public class InteriorOffice : IAdminastry, IPersonal
+    {
+        public void ChangeAddress()
+        {
+            
+        }
+
+        public void changePassport()
+        {
+            
+        }
+
+        public void GetEveryoneinfo()
+        {
+            
+        }
+
+        public void GetMyInfo()
+        {
+           
+        }
+
+        public void NotSomethingThatShouldBeAvilableForEveryone()
+        {
+            
+        }
+    }
+
+    public class ServiceMachine : IPersonal
+    {
+
+        private InteriorOffice InteriorOffice;
+
+        public ServiceMachine(InteriorOffice INO)
+        {
+            InteriorOffice = INO;
+
+        }
+
+        public void ChangeAddress()
+        {
+            InteriorOffice.ChangeAddress();
+        }
+
+        public void changePassport()
+        {
+            InteriorOffice.changePassport();
+        }
+
+        public void GetMyInfo()
+        {
+            InteriorOffice.GetMyInfo();
+        }
+    }
+}
